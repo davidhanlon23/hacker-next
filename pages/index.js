3eb797d4303c272cc453dbaa2bc413d7440da5ec
David Hanlon <david.hanlon@sbdinc.com>
2019-08-14 08:43:56 -0400

    added basic files

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import component from 'react';
+
+class Index extends component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Index;
